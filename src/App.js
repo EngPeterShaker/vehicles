b1add864e419ffe7d820f03fd96badad5aa88e56
pss <pss@crossworkers.com>
2019-10-23 16:47:19 +0200

    redux in action

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { connect } from "react-redux";
 
-function App() {
+import logo from "./logo.svg";
+import "./App.css";
+import { simpleAction } from "./actions/simpleAction";
+import VehiclesList from "./components/VehiclesList";
+
+function App(props) {
+
+  const simpleAction = event => {
+    props.simpleAction();
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+      {/* <header className="App-header">
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -18,9 +27,20 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+        <VehiclesList />
+      {/* <button onClick={() => simpleAction()}>Test redux action</button> */}
+      {/* <pre>{JSON.stringify(props)}</pre> */}
     </div>
   );
 }
-
-export default App;
+const mapStateToProps = state => ({
+  ...state
+});
+const mapDispatchToProps = dispatch => ({
+  simpleAction: () => dispatch(simpleAction())
+});
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(App);
--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,5 +1,7 @@
 import { combineReducers } from 'redux';
 import simpleReducer from './simpleReducer';
+import vehiclesReducer from './vehiclesReducer';
 export default combineReducers({
- simpleReducer
+ simpleReducer,
+vehiclesReducer
 });
